df6d9d42b8283ec74d220034b809ec7b683186c0
Dhruvi Ranwala <144709918+dhruvii-i@users.noreply.github.com>
2025-06-14 17:13:53 +0530

    Delete mental_health_dbt/models/mental_health_analysis directory

--- mental_health_dbt/models/mental_health_analysis/funnel_interview_openness.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    COUNT(*) AS open_to_interview,
-    SUM(CASE WHEN "anonymity" = 'Yes' THEN 1 ELSE 0 END) AS has_anonymity,
-    SUM(CASE WHEN "seek_help" = 'Yes' THEN 1 ELSE 0 END) AS seeks_help,
-    SUM(CASE WHEN "work_interfere" IN ('Never', 'Rarely') THEN 1 ELSE 0 END) AS low_interference
-FROM {{ ref('survey') }}
-WHERE "mental_health_interview" = 'Yes'
